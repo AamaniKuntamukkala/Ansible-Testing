86cafd28531fbc57ed0644ba3b03e68bc94587b5
Aamani Kuntamukkala <aamani344@gmail.com>
2025-12-03 16:20:38 +0530

    Update 7.ec2.tf

--- 7.ec2.tf
+++ 7.ec2.tf
@@ -1,25 +1,30 @@
-data "aws_ami" "my_ami" {
-  most_recent = true
-  name_regex  = "^DevSecOps"
-  owners      = ["211125710812"]
-}
+resource "azurerm_linux_virtual_machine" "webservers" {
+  count               = 3
+  name                = "${var.vnet_name}-PublicServer-${count.index + 1}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = lookup(var.instance_size, local.new_environment)
+  admin_username      = var.admin_username
+  network_interface_ids = [azurerm_network_interface.web_nic[count.index].id]
 
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
-resource "aws_instance" "webservers" {
-  #count                       = local.new_environment == "production" ? 3 : 1
-  count                       = 3
-  ami                         = data.aws_ami.my_ami.id
-  instance_type               = lookup(var.instance_type, local.new_environment)
-  key_name                    = var.key_name
-  subnet_id                   = element(aws_subnet.public-subnets.*.id, count.index)
-  vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
-  associate_public_ip_address = true
+  # Use custom image from Shared Image Gallery
+  source_image_id = data.azurerm_shared_image_version.latest.id
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.ssh_public_key
+  }
+
   tags = {
-    Name              = "${var.vpc_name}-PublicServer-${count.index + 1}"
+    Name              = "${var.vnet_name}-PublicServer-${count.index + 1}"
     Terraform-Managed = "Yes"
     Env               = local.new_environment
     ProjectID         = local.projid
     ManagedBy         = "Terraform"
   }
 }
-
