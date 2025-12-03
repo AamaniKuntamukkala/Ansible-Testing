29a5b12097478728d1145e2e88cd20113c7f5e30
Aamani Kuntamukkala <aamani344@gmail.com>
2025-12-03 16:15:51 +0530

    Update 5.public-routing.tf

--- 5.public-routing.tf
+++ 5.public-routing.tf
@@ -1,33 +1,26 @@
-resource "aws_route_table" "terraform-public" {
-  vpc_id = aws_vpc.default.id
+# Route Table (equivalent to AWS route table)
+resource "azurerm_route_table" "public" {
+  name                = "${var.vnet_name}-MAIN-RT"
+  location            = var.location
+  resource_group_name = var.resource_group_name
 
-  # route {
-  #   cidr_block = "0.0.0.0/0"
-  #   gateway_id = aws_internet_gateway.default.id
-  # }
+  route {
+    name           = "default-route"
+    address_prefix = "0.0.0.0/0"
+    next_hop_type  = "Internet"   # Equivalent to IGW in AWS
+  }
 
   tags = {
-    Name              = "${var.vpc_name}-MAIN-RT"
+    Name              = "${var.vnet_name}-MAIN-RT"
     Terraform-Managed = "Yes"
     Env               = local.new_environment
     ProjectID         = local.projid
   }
 }
 
-#VPC Peering Routes are getting recreated when we apply. To overcome this issue Routing Table
-#is created with out any routes & routes for igw,peering are created seperatly.
-#https://stackoverflow.com/questions/49174421/terraform-route-table-forcing-new-resource-every-apply
-
-resource "aws_route" "igw-route" {
-  route_table_id         = aws_route_table.terraform-public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.default.id
-}
-
-resource "aws_route_table_association" "terraform-public" {
-  #count             = 4 # 0 1 2
-  count = length(local.new_public_subnet_cidrs)
-  #Using * is called Splat Syntax
-  subnet_id      = element(aws_subnet.public-subnets.*.id, count.index)
-  route_table_id = aws_route_table.terraform-public.id
+# Associate route table with public subnets
+resource "azurerm_subnet_route_table_association" "public" {
+  count          = length(local.new_public_subnet_cidrs)
+  subnet_id      = azurerm_subnet.public_subnets[count.index].id
+  route_table_id = azurerm_route_table.public.id
 }
