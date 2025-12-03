9a3696bd6cbf10f2b30ff3f8b9357d946e72f248
Aamani Kuntamukkala <aamani344@gmail.com>
2025-12-03 16:22:21 +0530

    Migrate AWS security group to Azure NSG configuration

--- 8.sg.tf
+++ 8.sg.tf
@@ -1,47 +1,76 @@
-resource "aws_security_group" "allow_all" {
-  name        = "allow_all"
-  description = "Allow all inbound traffic"
-  vpc_id      = aws_vpc.default.id
+resource "azurerm_network_security_group" "allow_all" {
+  name                = "allow_all"
+  location            = var.location
+  resource_group_name = var.resource_group_name
 
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  # Allow ALL inbound traffic
+  security_rule {
+    name                       = "allow-all-inbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["10.1.1.0/32"]
+  # Allow SSH from 10.1.1.0/32
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "10.1.1.0/32"
+    destination_address_prefix = "*"
   }
 
-  ingress {
-    from_port   = 3389
-    to_port     = 3389
-    protocol    = "tcp"
-    cidr_blocks = ["10.1.1.0/32"]
+  # Allow RDP from 10.1.1.0/32
+  security_rule {
+    name                       = "allow-rdp"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "10.1.1.0/32"
+    destination_address_prefix = "*"
   }
 
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["10.2.1.0/32"]
+  # Allow MySQL from 10.2.1.0/32
+  security_rule {
+    name                       = "allow-mysql"
+    priority                   = 130
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3306"
+    source_address_prefix      = "10.2.1.0/32"
+    destination_address_prefix = "*"
   }
 
-
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  # Allow ALL outbound traffic
+  security_rule {
+    name                       = "allow-all-outbound"
+    priority                   = 100
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
-  # lifecycle {
-  #   ignore_changes = [
-  #     ingress,
-  #   ]
-  # }
+
+  tags = {
+    Terraform-Managed = "Yes"
+    Env               = local.new_environment
+    ProjectID         = local.projid
+  }
 }
